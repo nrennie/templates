47b89e8be01dc170b827ccd014bba9e99c4449fd
Nicola Rennie <42338476+nrennie@users.noreply.github.com>
2026-06-02 00:42:23 +0100

    Update tt-template.py

--- inst/tt/tt-template.py
+++ inst/tt/tt-template.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import plotnine as gg
 import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
 import PyDyTuesday
 import os
-from pyfonts import load_font
+import highlight_text as ht
+
 
 # Load data ---------------------------------------------------------------
 
@@ -13,19 +15,30 @@ tt_data = pd.read_csv()
 
 # Define colours ----------------------------------------------------------
 
-bg_col = ""
-text_col = ""
-highlight_col = ""
+bg_col = "#F2F4F8"
+text_col = "#151C28"
+highlight_col = "#7F055F"
 
 
 # Load fonts --------------------------------------------------------------
 
-title_font = load_font(
-   font_url="https://github.com/google/fonts/blob/main/apache/slackey/Slackey-Regular.ttf?raw=true"
-)
-body_font = load_font(
-   font_url="https://github.com/google/fonts/blob/main/ofl/archivo/Archivo%5Bwdth%2Cwght%5D.ttf?raw=true"
-)
+oswald_paths = [p for p in fm.findSystemFonts() if 'oswald' in p.lower()]
+for p in oswald_paths:
+    fm.fontManager.addfont(p)
+nunito_paths = [p for p in fm.findSystemFonts() if 'nunito' in p.lower()]
+for p in nunito_paths:
+    fm.fontManager.addfont(p)
+    
+available_names = [f.name for f in fm.fontManager.ttflist]
+if 'Nunito' in available_names:
+    body_font = 'Nunito'
+else:
+    body_font = 'sans-serif'
+
+if 'Oswald' in available_names:
+    title_font = 'Oswald'
+else:
+    title_font = 'sans-serif'
 
 
 # Data wrangling ----------------------------------------------------------
@@ -36,11 +49,26 @@ body_font = load_font(
 
 title = ""
 st = ""
-cap = ""
+cap = '<Data::{"fontweight": "bold"}>: source\n<Graphic::{"fontweight": "bold"}>: Nicola Rennie (@nrennie)'
 
 
 # Plot --------------------------------------------------------------------
 
+p = ()
+
+fig = p.draw()
+fig.set_size_inches(7, 5, forward=True)
+
+plt.figure(fig)
+
+ht.fig_text(
+    x=0.01, y=0.06, s=cap,
+    fontsize=7.5, va='top',
+    fontfamily=body_font,
+    color=text_col,
+    linespacing=0.6,
+    fig=fig)
+    
 
 # Save --------------------------------------------------------------------
 
